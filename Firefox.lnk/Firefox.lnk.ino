67a0df9dfa36e152f9b3bc4cf40f8fd0dc2c5bf9
Alice <alice.gazzola@barsanti.edu.it>
2019-02-16 11:08:37 +0100

    16/02/2019 Verifica

---
+++ Firefox.lnk/Firefox.lnk.ino
@@ -0,0 +1,65 @@
+int ledverde = 8;
+int ledrosso =11;
+int ledgiallo=10;
+int ledbianco=9;
+int i=0;
+int durata=random(100,1000);
+int sequenza;
+void setup() {
+pinMode(ledverde,OUTPUT);
+pinMode(ledrosso,OUTPUT);
+pinMode(ledgiallo,OUTPUT);
+pinMode(ledbianco,OUTPUT);
+Serial.begin(9600);
+chiedocicli();
+//chiedodurata();
+}
+void chiedocicli(){
+  Serial.println("Scrivi quante deve eseguire la sequenza");
+  while(Serial.available()==0);
+    if ( Serial.available ()> 0){
+      sequenza=Serial.readString().toInt();
+       Serial.println(sequenza);
+      }
+  
+}
+/*void chiedodurata (){
+     Serial.println("Scrivi quanto deve restare accesso il led");
+  while(Serial.available()==0);
+    if ( Serial.available ()> 0){
+      durata= Serial.readString().toInt();
+      Serial.println(durata);
+       Serial.println(durata*2);
+        Serial.println(durata*3);
+      
+      }
+    }*/
+   
+    
+void programma (){
+   digitalWrite(ledrosso,HIGH);
+   delay (durata);
+    digitalWrite(ledrosso,LOW);
+     delay (durata);
+     digitalWrite(ledgiallo,HIGH);
+   delay (durata*2);
+    digitalWrite(ledgiallo,LOW);
+     delay (durata);
+      digitalWrite(ledbianco,HIGH);
+   delay (durata);
+    digitalWrite(ledbianco,LOW);
+     delay (durata);
+      digitalWrite(ledverde,HIGH);
+   delay (durata*3);
+    digitalWrite(ledverde,LOW);
+     delay (durata);
+     
+   
+  }
+
+void loop() {
+ while (i<sequenza){
+programma();
+i++;}
+setup();
+}
